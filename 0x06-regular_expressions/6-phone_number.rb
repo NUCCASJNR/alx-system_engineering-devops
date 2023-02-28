73157e6231bac81da5c26464f054a58ce79418c8
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-02-28 23:55:11 +0100

    create a Ruby script that accepts one argument and pass it to a regular expression matching method

---
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+#This task is brought to you by a professional advisor Neha Jain, Senior Software Engineer at LinkedIn.
+#
+#Requirement:
+#
+#The regular expression must match a 10 digit phone number
+
+puts ARGV[0].scan(/^\d{10}$/)$
