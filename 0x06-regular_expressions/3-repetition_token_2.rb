2d4c54ad55fc61343b349f7cae06d3404e6b1b66
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-02-28 22:58:01 +0100

    create a Ruby script that accepts one argument and pass it to a regular expression matching method

--- 0x06-regular_expressions/3-repetition_token_2.rb
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -4,4 +4,4 @@
 #Find the regular expression that will match the above cases
 #Using the project instructions, create a Ruby script that accepts one argument #and pass it to a regular expression matching method
 
-puts ARGV[0].scan(/hbt{1,4}n/)
+puts ARGV[0].scan(/hbt+n/)
