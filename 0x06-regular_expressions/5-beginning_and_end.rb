c1f5a1c9565c8891841dfc3b166cc7a3e02df04a
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-02-28 23:31:34 +0100

    create a Ruby script that accepts one argument and pass it to a regular expression matching method

---
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+
+#The regular expression must be exactly matching a string that starts with h ends with n and can have any single character in between
+#Using the project instructions, create a Ruby script that accepts one argument and pass it to a regular expression matching method
+
+puts ARGS[0].scan(/^h.n$/)
