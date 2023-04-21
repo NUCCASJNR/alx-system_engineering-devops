5bc2988ca8a4dab684afc02f141c5f5553142095
root <root@AlAreef.localdomain>
2023-04-21 23:08:44 +0100

    I still love puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,4 +1,14 @@
+"Using Puppet, create a file in /tmp.
+
+Requirements:
+
+File path is /tmp/school
+File permission is 0744
+File owner is www-data
+File group is www-data
+File contains I love Puppet
 file { '/tmp/school':
+"
   ensure  => 'present',
   mode    => '0744',
   owner   => 'www-data',
