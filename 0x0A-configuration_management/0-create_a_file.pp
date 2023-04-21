04046530fd43174e8a3c07f6df3511ae91616695
root <root@AlAreef.localdomain>
2023-04-21 23:11:05 +0100

    update puppet error

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,14 +1,15 @@
-"Using Puppet, create a file in /tmp.
+#Using Puppet, create a file in /tmp.
 
-Requirements:
+#Requirements:
 
-File path is /tmp/school
-File permission is 0744
-File owner is www-data
-File group is www-data
-File contains I love Puppet
+#File path is /tmp/school
+#File permission is 0744
+#File owner is www-data
+#File group is www-data
+#File contains I love Puppet
+
 file { '/tmp/school':
-"
+
   ensure  => 'present',
   mode    => '0744',
   owner   => 'www-data',
