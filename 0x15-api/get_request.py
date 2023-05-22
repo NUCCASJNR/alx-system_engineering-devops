68a208699e6074b24815de275eba6b12ccefc866
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-05-22 16:41:11 +0100

    get request

---
+++ 0x15-api/get_request.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import requests
+import sys
+
+user = sys.argv[1]
+url = f"https://api.github.com/users/{user}"
+response = requests.get(url)
+if response.status_code == 200:
+    data = response.json()
+    print(f"{data.get('id')}")
+else:
+    print('Error:', response.status_code)
