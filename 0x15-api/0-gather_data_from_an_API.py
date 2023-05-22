f2553e0040b3b6b6aa6931a36964cda4f16190f3
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-05-22 19:01:25 +0100

    fetching data from anapio

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+GAther data from an API
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    emp_id = sys.argv[1]
+    url = f"https://jsonplaceholder.typicode.com/users/{emp_id}"
+    res = requests.get(url)
+    new_url = url + "/todos"
+    new_res = requests.get(new_url)
+    if new_res.status_code == 200:
+        data = res.json()
+        name = f"{data.get('name')}"
+        tasks = new_res.json()
+        # print(tasks)
+        real = []
+        for i in tasks:
+            if i.get("completed") is True:
+                real.append(i)
+        # print(real)
+        titles = []
+        for task in tasks:
+            if task.get("completed") is True:
+                titles.append(task.get("title"))
+        done = f"Employee {name} is done with tasks"
+        print("{} {}/{}:".format(done, len(real), len(tasks)))
+        for tit in titles:
+            print("\t {}".format(tit))
+    else:
+        print(f"Error: {res.status_code}")
--- 0x15-api/get_request.py
+++ 0x15-api/get_request.py
@@ -4,7 +4,8 @@ import requests
 import sys
 
 user = sys.argv[1]
-url = f"https://api.github.com/users/{user}"
+#url = f"https://api.github.com/users/{user}"
+url = f"https://jsonplaceholder.typicode.com/todos/{user}"
 response = requests.get(url)
 if response.status_code == 200:
     data = response.json()
