89fb6a4bd7cc0db5b485871e8354291b1a0b3244
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-06-06 23:55:39 +0100

    reddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 """
- a function that queries the Reddit API and returns the number
- of subscribers (not active users, total subscribers)
- for a given subreddit. If an invalid subreddit is given,
- the function should return 0.
+Queries the reddit Api
 """
 
 import requests
