ab1ec43d9666bd0331a6a880f17b0ea32f188678
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-06-07 01:55:05 +0100

    idan

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -5,20 +5,25 @@
 import requests
 
 
-def recurse(subreddit, hot_list=[]):
-    """Returns a list containing the titles of all hot articles for a subreddit"""
+def recurse(subreddit, hot_list=[], after=None):
+    """Returns a list containing the titles of all
+      hot articles for a subreddit"""
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    if hot_list == []:
+        hot_list = []
     headers = {
             "User-Agent": "Fake Agent"
             }
-    response = requests.get(url, headers=headers)
+    params = {'after': after} if after else {}
+    response = requests.get(url, headers=headers, params=params)
     outcome = response.json()
     if response.status_code == 200:
         for sub in outcome['data']['children']:
             hot_list.append(sub['data']['title'])
-        if outcome['data']['after'] is not None:
-            return recurse(subreddit, hot_list)
+        after = outcome['data']['after']
+        if after:
+            return recurse(subreddit, hot_list, after)
         else:
             return hot_list
     else:
-        return None
+        return Nonei
