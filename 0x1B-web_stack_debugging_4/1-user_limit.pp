4500d532f0268f775a7e1733c59705380d8a8329
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-06-15 23:17:37 +0100

    ye ye to devops

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,8 @@
+# Increase sytem open files
+exec {'increase_hard':
+command => '/bin/sed -i "s/holberton hard nofile 5/holberton hard nofile 65535/g" /etc/security/limits.conf',
+}
+
+exec {'increase_soft':
+command => '/bin/sed -i "s/holberton soft nofile 4/holberton soft nofile 65535/g" /etc/security/limits.conf',
+}
